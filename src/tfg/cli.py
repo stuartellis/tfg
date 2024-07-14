30908dc6444f0d458f4f57533aded49328751246
Stuart Ellis <stuart@stuartellis.name>
2024-07-14 09:03:48 +0100

    feat: Detect OpenTofu

--- src/tfg/cli.py
+++ src/tfg/cli.py
@@ -18,6 +18,7 @@ import subprocess
 import sys
 from os import environ, pathsep
 from pathlib import Path
+from shutil import which
 from string import Template
 from typing import Any
 
@@ -49,10 +50,12 @@ TEMPLATE_SUB_COMMANDS = {
     "version": "$tf_exe version",
 }
 
+TF_EXES = ["terraform", "tofu"]
+
 ## Functions for template context
 
 
-def build_config(
+def build_config_dict(
     env_vars: dict[str, str],
     path_set: dict[str, Path],
 ) -> dict[str, str]:
@@ -97,8 +100,8 @@ def build_tf_vars_files_opt(config: dict[str, str]) -> str:
     return f"-var-file={config['tf_envs_dir']}/all/{config['stack_name']}.tfvars -var-file={config['tf_env_dir']}/{config['stack_name']}.tfvars"
 
 
-def build_tf_vars_opt(config: dict[str, str]) -> str:
-    """Return vars for TF."""
+def build_tf_vars_extras_opt(config: dict[str, str]) -> str:
+    """Return extra vars for TF."""
     names = ["environment", "stack_name", "variant"]
     opts = [f"-vars={name}={config[name]}" for name in names]
     return " ".join(opts)
@@ -121,13 +124,13 @@ def tf_backend_type() -> str:
 def tf_context(config: dict[str, str]) -> dict[str, str]:
     """Return context for TF commands."""
     context = config.copy()
-    context["tf_exe"] = tf_exe()
+    context["tf_exe"] = tf_exe(TF_EXES)
 
     context["tf_plan_path"] = pathsep.join(
         [config["tf_tmp_dir"], build_tf_plan_name(context)]
     )
     context["tf_vars_files_opt"] = build_tf_vars_files_opt(config)
-    context["tf_vars_opt"] = build_tf_vars_opt(config)
+    context["tf_vars_opt"] = build_tf_vars_extras_opt(config)
 
     if tf_remote_backend_required(config):
         backend_type = tf_backend_type()
@@ -141,12 +144,24 @@ def tf_context(config: dict[str, str]) -> dict[str, str]:
     return context
 
 
-def tf_exe() -> str:
+def tf_exe(tf_names: list[str]) -> str:
     """Return executable for TF."""
-    return "terraform"
+    for tf_name in tf_names:
+        if which(tf_name):
+            return tf_name
+
+    err = f"No TF executable found. Ensure that PATH has one of these: {', '.join(tf_names)}."
+    raise FileNotFoundError(err)
 
 
 ### Functions for command-line
+
+
+def cli() -> None:
+    """Run with command-line options."""
+    parser = build_arg_parser(version=VERSION, subcommands=[*TEMPLATE_SUB_COMMANDS])
+    opts = vars(parser.parse_args())
+    run(opts)
 
 
 def build_arg_parser(version: str, subcommands: list[str]) -> argparse.ArgumentParser:
@@ -176,10 +191,30 @@ def build_arg_parser(version: str, subcommands: list[str]) -> argparse.ArgumentP
     return parser
 
 
-def get_env_vars(required_vars: list[str], optional_vars: list[str]) -> dict[str, str]:
-    """Return object of environment variables."""
-    env_variables = [*required_vars, *optional_vars]
-    return {k: v for k, v in environ.items() if k in env_variables}
+def build_config() -> dict[Any, Any]:
+    """Build the full configuration object."""
+    env_vars = get_env_vars(REQUIRED_VARS, OPTIONAL_VARS)
+    missing_vars = check_env_vars(env_vars, REQUIRED_VARS)
+    if len(missing_vars) > 0:
+        print(f"Missing required variables: {', '.join(missing_vars)}")
+        sys.exit(1)
+
+    project_root_dir = Path.cwd()
+    path_set = build_path_set(
+        project_root_dir, env_vars["ENVIRONMENT"], env_vars["STACK_NAME"]
+    )
+
+    config = build_config_dict(env_vars, path_set)
+
+    if "variant" not in config:
+        config["variant"] = "default"
+
+    return config
+
+
+def check_env_vars(env_vars: dict[str, str], required_vars: list[str]) -> list[str]:
+    """Return all of the required variables that are not present."""
+    return [r_var for r_var in required_vars if r_var not in env_vars]
 
 
 def execute_cmd_string(cmd: str) -> subprocess.CompletedProcess:
@@ -187,9 +222,10 @@ def execute_cmd_string(cmd: str) -> subprocess.CompletedProcess:
     return subprocess.run(cmd, check=True, shell=True)  # noqa: S602
 
 
-def check_env_vars(env_vars: dict[str, str], required_vars: list[str]) -> list[str]:
-    """Return all of the required variables that are not present."""
-    return [r_var for r_var in required_vars if r_var not in env_vars]
+def get_env_vars(required_vars: list[str], optional_vars: list[str]) -> dict[str, str]:
+    """Return object of environment variables."""
+    env_variables = [*required_vars, *optional_vars]
+    return {k: v for k, v in environ.items() if k in env_variables}
 
 
 def info() -> dict[str, str]:
@@ -202,7 +238,7 @@ def info() -> dict[str, str]:
 
     return {
         "python_version": ".".join([str(v) for v in python_version]),
-        "tf_exe": tf_exe(),
+        "tf_exe": tf_exe(TF_EXES),
         "tfg_version": VERSION,
     }
 
@@ -211,13 +247,6 @@ def load_json(file_path: Path) -> Any:  # noqa: ANN401
     """Return variables from JSON."""
     with Path.open(file_path) as f_in:
         return json.load(f_in)
-
-
-def cli() -> None:
-    """Run with command-line options."""
-    parser = build_arg_parser(version=VERSION, subcommands=[*TEMPLATE_SUB_COMMANDS])
-    opts = vars(parser.parse_args())
-    run(opts)
 
 
 def print_debug_info(options: dict[str, Any], context: dict[str, Any]) -> None:
@@ -238,44 +267,30 @@ def run(options: dict[str, Any]) -> None:
         sys.stderr.write("Arguments are required")
         sys.exit(1)
     else:
-        env_vars = get_env_vars(REQUIRED_VARS, OPTIONAL_VARS)
-        missing_vars = check_env_vars(env_vars, REQUIRED_VARS)
-        if len(missing_vars) > 0:
-            print(f"Missing required variables: {', '.join(missing_vars)}")
-            sys.exit(1)
-
-        project_root_dir = Path.cwd()
-        path_set = build_path_set(
-            project_root_dir, env_vars["ENVIRONMENT"], env_vars["STACK_NAME"]
-        )
-
-        config = build_config(env_vars, path_set)
-
-        if "variant" not in config:
-            config["variant"] = "default"
-
+        config = build_config()
         cmd_context = tf_context(config)
 
         if options["debug"]:
             print_debug_info(options, cmd_context)
 
-        template = TEMPLATE_SUB_COMMANDS[options["subcommand"]]
-        cmd = render_cmd_string(cmd_context, template)
+        if options["subcommand"] in TEMPLATE_SUB_COMMANDS:
+            template = TEMPLATE_SUB_COMMANDS[options["subcommand"]]
+            cmd = render_cmd_string(cmd_context, template)
 
-        if options["print"]:
-            print(cmd)
-        else:
-            try:
-                execute_cmd_string(cmd)
-            except FileNotFoundError as exc:
-                print(
-                    f"Process failed because the executable could not be found.\n{exc}"
-                )
-            except subprocess.CalledProcessError as exc:
-                print(
-                    f"Process failed because did not return a successful return code. "
-                    f"Returned {exc.returncode}\n{exc}"
-                )
+            if options["print"]:
+                print(cmd)
+            else:
+                try:
+                    execute_cmd_string(cmd)
+                except FileNotFoundError as exc:
+                    print(
+                        f"Process failed because the executable could not be found.\n{exc}"
+                    )
+                except subprocess.CalledProcessError as exc:
+                    print(
+                        f"Process failed because did not return a successful return code. "
+                        f"Returned {exc.returncode}\n{exc}"
+                    )
 
 
 """Run the main() function when this file is executed"""
